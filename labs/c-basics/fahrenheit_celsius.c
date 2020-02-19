442356e4ce5d8422082156f07d3a63628c30768e
Miguel Barro <miguelbarroe@gmail.com>
2020-02-18 20:15:58 -0600

    Solve c-basics lab
    Signed-off-by: Miguel Barro <miguelbarroe@gmail.com>

--- labs/c-basics/fahrenheit_celsius.c
+++ labs/c-basics/fahrenheit_celsius.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
-#define   LOWER  0       /* lower limit of table */
-#define   UPPER  300     /* upper limit */
-#define   STEP   20      /* step size */
-
 /* print Fahrenheit-Celsius table */
 
-int main()
+int main(int argc, char *argv[])
 {
-    int fahr;
+    if(argc == 2) {
+        printf("Fahrenheit: %3d, Celcius: %6.1f\n", atoi(argv[1]), (5.0/9.0)*(atoi(argv[1])-32));
+    } else if(argc == 4) {
+        int fahr;
 
-    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
-	printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+        for (fahr = atoi(argv[1]); fahr <= atoi(argv[2]); fahr = fahr + atoi(argv[3]))
+	        printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+    } else {
+        printf("Wrong number of arguments!");
+    }
 
     return 0;
 }
