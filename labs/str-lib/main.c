f5ef088deb3fe7b6ac6b14cf3ed4fc6f61cf5c1a
Miguel Barro <miguelbarroe@gmail.com>
2020-03-22 18:30:18 -0600

    Solve str-lib
    Signed-off-by: Miguel Barro <miguelbarroe@gmail.com>

--- labs/str-lib/main.c
+++ labs/str-lib/main.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
-int main() {
+int mystrlen(char *str);
+char *mystradd(char *origin, char *addition);
+int mystrfind(char *origin, char *substr);
+
+int main(int argc, char *argv[])
+{
+    if(argc < 4) {
+        printf("Invalid number of arguments.");
+        return 1;
+    }
+
+    char *concat = mystradd(argv[1], argv[2]);
+    int find = mystrfind(concat, argv[3]);
+
+    printf("Initial Lenght      : %d\n", mystrlen(argv[1]));
+    printf("New String          : %s\n", concat);
+    if(find == -1) {
+        printf("SubString was found : no\n");
+    } else {
+        printf("SubString was found : yes, on index %d\n", find);
+    }
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,56 @@
-
+#include <stdlib.h>
 
 int mystrlen(char *str){
-    return 0;
+    int c, i;
+
+    i = 0;
+
+    while((c = str[i]) != '\0') {
+        i++;
+    }    
+
+    return i;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    int l1, l2, i;
+
+    l1 = mystrlen(origin);
+    l2 = mystrlen(addition);
+
+    char *res = malloc(sizeof(char) * (l1 + l2 + 1));
+
+    for(i = 0; i < l1; i++) {
+        res[i] = origin[i];
+    }
+    for(i = 0; i < l2; i++) {
+        res[i + l1] = addition[i];
+    } 
+    res[l1 + l2] = '\0';
+
+    return res;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+    int l1, l2, sameChar, i, j;
+
+    l1 = mystrlen(origin);
+    l2 = mystrlen(substr);
+
+    for(i = 0; i < l1 - l2 + 1; i++) {
+        sameChar = 0;
+        for(j = i; j < l2 + i; j++) {
+            if(origin[j] == substr[j - i]) {
+                sameChar++;
+            } else {
+                break;
+            }
+
+            if(sameChar == l2) {
+                return i;
+            }
+        }
+    }
+
+    return -1;
 }
