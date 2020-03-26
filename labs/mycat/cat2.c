6326b892c1d32d101f331d2df242c9fb0b1313ef
Miguel Barro <miguelbarroe@gmail.com>
2020-03-25 18:57:11 -0600

    Solve mycat
    Signed-off-by: Miguel Barro <miguelbarroe@gmail.com>

--- labs/mycat/cat2.c
+++ labs/mycat/cat2.c
@@ -1,33 +1,36 @@
+#include <fcntl.h>
 #include <stdio.h>
+#include <unistd.h>
+
+int myRead(int file);
+void myWrite(int c, int file);
 
 /* filecopy:  copy file ifp to file ofp */
-void filecopy(FILE *ifp, FILE *ofp)
+void filecopy(int ifp, int ofp)
 {
     int c;
 
-    while ((c = getc(ifp)) != EOF)
-        putc(c, ofp);
-
+    while ((c = myRead(ifp)) != EOF)
+        myWrite(c, ofp);
 }
 
 /* cat:  concatenate files, version 2 */
 int main(int argc, char *argv[])
 {
-    FILE *fp;
-    void filecopy(FILE *, FILE *);
+    int fp;
+    void filecopy(int ifp, int ofp);
     char *prog = argv[0];   /* program name for errors */
 
     if (argc == 1)  /* no args; copy standard input */
-        filecopy(stdin, stdout);
+        filecopy(fileno(stdin), fileno(stdout));
     else
         while (--argc > 0)
-            if ((fp = fopen(*++argv, "r")) == NULL) {
-                fprintf(stderr, "%s: can′t open %s\n",
-			prog, *argv);
+            if ((fp = open(*++argv, O_RDONLY, 0600)) < 0) {
+                fprintf(stderr, "%s: can′t open %s\n", prog, *argv);
                 return 1;
             } else {
-                filecopy(fp, stdout);
-                fclose(fp);
+                filecopy(fp, fileno(stdout));
+                close(fp);
             }
 
     if (ferror(stdout)) {
@@ -36,4 +39,20 @@ int main(int argc, char *argv[])
     }
 
     return 0;
+}
+
+int myRead(int file)
+{
+    int buffer[1];
+
+    if(read(file, buffer, 1) == 0) {
+        return EOF;
+    }
+
+    return buffer[0];
+}
+
+void myWrite(int c, int file)
+{
+    write(file, &c, 1);
 }
