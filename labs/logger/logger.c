052da95cb7c857feffd8fe30cac3dda52f376124
Miguel Barro <miguelbarroe@gmail.com>
2020-04-14 19:17:47 -0700

    Solve logger
    Signed-off-by: Miguel Barro <miguelbarroe@gmail.com>

--- labs/logger/logger.c
+++ labs/logger/logger.c
@@ -1,6 +1,7 @@
-#include<stdio.h>
-#include<stdarg.h>
+#include <stdio.h>
+#include <stdarg.h>
 #include <stdlib.h>
+#include <signal.h>
 
 #define RESET		0
 #define BRIGHT 		1
@@ -19,24 +20,6 @@
 #define CYAN		6
 #define WHITE		7
 
-char *convert(unsigned int num, int base)
-{
-	static char *representation= "0123456789ABCDEF";
-	static char buffer[50];
-	char *ptr;
-
-	ptr = &buffer[49];
-	*ptr = '\0';
-	
-	do
-	{
-		*--ptr = representation[num%base];
-		num /= base;
-	} while(num != 0);
-	
-	return ptr;
-}
-
 void textcolor(int attr, int fg, int bg)
 {
 	char command[13];
@@ -45,266 +28,55 @@ void textcolor(int attr, int fg, int bg)
 	printf("%s", command);
 }
 
-int infof(char* format, ...)
+int infof(const char* format, ...)
 {
-	char *traverse, *s;
-	char buffer[1000];
-	int i;
-	double f;
-	
-	va_list arg;
-	va_start(arg, format);
-
 	textcolor(BRIGHT, BLUE, BLACK);
-	
-	for(traverse = format; *traverse != '\0'; traverse++)
-	{
-		while(*traverse != '%')
-		{
-			if(*traverse == '\0')
-			{
-				va_end(arg);
-    			return 4;
-			}
-
-			putchar(*traverse);
-			traverse++;
-		}
-		
-		traverse++;
-		
-		switch(*traverse)
-		{
-			case 'c' : i = va_arg(arg, int);
-						putchar(i);
-						break;
-			case 'd' : i = va_arg(arg, int);
-						if(i<0)
-						{
-							i = -i;
-							putchar('-');
-						}
-						fputs(convert(i, 10), stdout);
-						break;
-			case 'o': i = va_arg(arg, unsigned int);
-						fputs(convert(i, 8), stdout);
-						break;
-			case 's': s = va_arg(arg, char *);
-						fputs(s, stdout);
-						break;
-			case 'x': i = va_arg(arg, unsigned int);
-						fputs(convert(i, 16), stdout);
-						break;
-			case 'f': f = va_arg(arg, double);
-						fputs(gcvt(f, 10, buffer), stdout);
-						break;
-			case '%': putchar('%');
-						break;
-			default: printf("Invalid formatting!\n");
-						va_end(arg);
-    					return 4;
-		}
-	}
-
-	va_end(arg);
-	textcolor(RESET, WHITE, BLACK);
-    return 4;
-}
-
-int warnf(char* format, ...)
-{
-	char *traverse, *s;
-	char buffer[1000];
-	int i;
-	double f;
-	
 	va_list arg;
 	va_start(arg, format);
 
+	vprintf(format, arg);
+
+	va_end(arg);
+	textcolor(RESET, WHITE, BLACK);
+	return 4;
+}
+
+int warnf(const char* format, ...)
+{
 	textcolor(BRIGHT, YELLOW, BLACK);
-	
-	for(traverse = format; *traverse != '\0'; traverse++)
-	{
-		while(*traverse != '%')
-		{
-			if(*traverse == '\0')
-			{
-				va_end(arg);
-    			return 5;
-			}
-
-			putchar(*traverse);
-			traverse++;
-		}
-		
-		traverse++;
-		
-		switch(*traverse)
-		{
-			case 'c' : i = va_arg(arg, int);
-						putchar(i);
-						break;
-			case 'd' : i = va_arg(arg, int);
-						if(i<0)
-						{
-							i = -i;
-							putchar('-');
-						}
-						fputs(convert(i, 10), stdout);
-						break;
-			case 'o': i = va_arg(arg, unsigned int);
-						fputs(convert(i, 8), stdout);
-						break;
-			case 's': s = va_arg(arg, char *);
-						fputs(s, stdout);
-						break;
-			case 'x': i = va_arg(arg, unsigned int);
-						fputs(convert(i, 16), stdout);
-						break;
-			case 'f': f = va_arg(arg, double);
-						fputs(gcvt(f, 10, buffer), stdout);
-						break;
-			case '%': putchar('%');
-						break;
-			default: printf("Invalid formatting!\n");
-						va_end(arg);
-    					return 5;
-		}
-	}
-
-	va_end(arg);
-	textcolor(RESET, WHITE, BLACK);
-    return 5;
-}
-
-int errorf(char* format, ...)
-{
-	char *traverse, *s;
-	char buffer[1000];
-	int i;
-	double f;
-	
 	va_list arg;
 	va_start(arg, format);
 
+	vprintf(format, arg);
+
+	va_end(arg);
+	textcolor(RESET, WHITE, BLACK);
+	return 5;
+}
+
+int errorf(const char* format, ...)
+{
 	textcolor(BRIGHT, RED, BLACK);
-	
-	for(traverse = format; *traverse != '\0'; traverse++)
-	{
-		while(*traverse != '%')
-		{
-			if(*traverse == '\0')
-			{
-				va_end(arg);
-    			return 6;
-			}
-
-			putchar(*traverse);
-			traverse++;
-		}
-		
-		traverse++;
-		
-		switch(*traverse)
-		{
-			case 'c' : i = va_arg(arg, int);
-						putchar(i);
-						break;
-			case 'd' : i = va_arg(arg, int);
-						if(i<0)
-						{
-							i = -i;
-							putchar('-');
-						}
-						fputs(convert(i, 10), stdout);
-						break;
-			case 'o': i = va_arg(arg, unsigned int);
-						fputs(convert(i, 8), stdout);
-						break;
-			case 's': s = va_arg(arg, char *);
-						fputs(s, stdout);
-						break;
-			case 'x': i = va_arg(arg, unsigned int);
-						fputs(convert(i, 16), stdout);
-						break;
-			case 'f': f = va_arg(arg, double);
-						fputs(gcvt(f, 10, buffer), stdout);
-						break;
-			case '%': putchar('%');
-						break;
-			default: printf("Invalid formatting!\n");
-						va_end(arg);
-    					return 6;
-		}
-	}
-
-	va_end(arg);
-	textcolor(RESET, WHITE, BLACK);
-    return 6;
-}
-
-int panicf(char* format, ...)
-{
-	char *traverse, *s;
-	char buffer[1000];
-	int i;
-	double f;
-	
 	va_list arg;
 	va_start(arg, format);
 
-	textcolor(BRIGHT, WHITE, BLACK);
-	
-	for(traverse = format; *traverse != '\0'; traverse++)
-	{
-		while(*traverse != '%')
-		{
-			if(*traverse == '\0')
-			{
-				va_end(arg);
-    			return 100;
-			}
-
-			putchar(*traverse);
-			traverse++;
-		}
-		
-		traverse++;
-		
-		switch(*traverse)
-		{
-			case 'c' : i = va_arg(arg, int);
-						putchar(i);
-						break;
-			case 'd' : i = va_arg(arg, int);
-						if(i<0)
-						{
-							i = -i;
-							putchar('-');
-						}
-						fputs(convert(i, 10), stdout);
-						break;
-			case 'o': i = va_arg(arg, unsigned int);
-						fputs(convert(i, 8), stdout);
-						break;
-			case 's': s = va_arg(arg, char *);
-						fputs(s, stdout);
-						break;
-			case 'x': i = va_arg(arg, unsigned int);
-						fputs(convert(i, 16), stdout);
-						break;
-			case 'f': f = va_arg(arg, double);
-						fputs(gcvt(f, 10, buffer), stdout);
-						break;
-			case '%': putchar('%');
-						break;
-			default: printf("Invalid formatting!\n");
-						va_end(arg);
-    					return 100;
-		}
-	}
+	vprintf(format, arg);
 
 	va_end(arg);
 	textcolor(RESET, WHITE, BLACK);
-    return 100;
+	return 6;
+}
+
+int panicf(const char* format, ...)
+{
+	textcolor(BRIGHT, RED, BLACK);
+	va_list arg;
+	va_start(arg, format);
+
+	vprintf(format, arg);
+
+	va_end(arg);
+	textcolor(RESET, WHITE, BLACK);
+	raise (SIGABRT);
+	exit(1);
 }
--- labs/logger/testLogger.c
+++ labs/logger/testLogger.c
@@ -8,15 +8,11 @@ int panicf(const char *format, ...);
 int main(int argc, char const *argv[])
 {
     infof("Hello World! INFO:\nchar: %c\nbase 10 int: %d\nbase 8 int: %o\nstring: %s\nbase 16 int: %x\nfloat: %f\npercentage sign: %%\n", 'a', -123, 10, "holis", 100, 64.64);
-    infof("Format error: %m\n");
 
     warnf("Hello World! WARNING:\nchar: %c\nbase 10 int: %d\nbase 8 int: %o\nstring: %s\nbase 16 int: %x\nfloat: %f\npercentage sign: %%\n", 'a', -123, 10, "holis", 100, 64.64);
-    warnf("Format error: %m\n");
 
     errorf("Hello World! ERROR:\nchar: %c\nbase 10 int: %d\nbase 8 int: %o\nstring: %s\nbase 16 int: %x\nfloat: %f\npercentage sign: %%\n", 'a', -123, 10, "holis", 100, 64.64);
-    errorf("Format error: %m\n");
 
     panicf("Hello World! PANIC:\nchar: %c\nbase 10 int: %d\nbase 8 int: %o\nstring: %s\nbase 16 int: %x\nfloat: %f\npercentage sign: %%\n", 'a', -123, 10, "holis", 100, 64.64);
-    panicf("Format error: %m\n");
     return 0;
 }
